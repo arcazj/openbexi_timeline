800f8493546c52b05a33416c7a52df9db755d364
arcazj <17037788+arcazj@users.noreply.github.com>
2020-04-12 19:52:33 -0400

    Add files via upload
    Add THREE.TextSprite lib.

---
+++ THREE.TextSprite/src/index.js
@@ -0,0 +1,31 @@
+import Class from './Class';
+import classPrototypeMembers from './classPrototypeMembers';
+
+let {prototype} = Class;
+
+[
+	'align',
+	'fillStyle',
+	'fontFamily',
+	'fontStyle',
+	'fontVariant',
+	'fontWeight',
+	'lineGap',
+	'padding',
+	'strokeStyle',
+	'strokeWidth',
+	'text',
+].forEach(property => {
+	Object.defineProperty(prototype, property, {
+		get() {
+			return this.material.map[property];
+		},
+		set(value) {
+			this.material.map[property] = value;
+		},
+	});
+});
+
+Object.assign(prototype, classPrototypeMembers);
+
+export default Class;
