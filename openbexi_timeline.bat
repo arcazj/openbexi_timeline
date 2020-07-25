a614ed8a38dba54ea44bfb393bb3effe89f8c3fa
ARCAZJ <arcazj@aol.com>
2020-07-25 13:35:05 -0400

    Add updated tools and updated scripts.  Renamed openbexi_timeline Tomcat server.

---
+++ openbexi_timeline.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+
+::----------------------------------------------------------------------
+:: openBEXI_timeline Web serve startup script example.
+::----------------------------------------------------------------------
+
+:: ---------------------------------------------------------------------
+:: Locate a JDK installation directory which will be used to run topenBEXI_timeline.
+:: Try (in order):  ..\jre, JDK_HOME, JAVA_HOME.
+:: ---------------------------------------------------------------------
+SET JDK=
+SET BITS=
+
+:: Set your environment variables here if needed
+SET JDK_HOME=
+SET OPENBEXI_TIMELINE_HOME=
+
+IF EXIST "%JDK_HOME%" SET JDK=%JDK_HOME%
+IF EXIST "%JDK%" GOTO check
+
+IF EXIST "%JAVA_HOME%" SET JDK=%JAVA_HOME%
+
+:check
+SET JAVA_EXE=%JDK%\bin\java.exe
+IF NOT EXIST "%JAVA_EXE%" SET JAVA_EXE=%JDK%\jre\bin\java.exe
+IF NOT EXIST "%JAVA_EXE%" (
+  ECHO ERROR: cannot start openbexi_timeline.
+  ECHO No JDK found. Please validate either JDK_HOME or JAVA_HOME points to valid JDK installation.
+  EXIT /B
+)
+
+SET JRE=%JDK%
+IF EXIST "%JRE%\jre" SET JRE=%JDK%\jre
+IF EXIST "%JRE%\lib\amd64" (
+  SET BITS=64
+) ELSE (
+  IF EXIST "%JRE%\bin\windowsaccessbridge-64.dll" SET BITS=64
+)
+
+
+SET CLASS_PATH=%OPENBEXI_TIMELINE_HOME%\lib\openbexi_timeline.jar
+echo %CLASS_PATH%
+
+
+:: ---------------------------------------------------------------------
+:: Run openBEXI_timeline.
+:: ---------------------------------------------------------------------
+
+"%JAVA_EXE%" -cp "%CLASS_PATH%" com.openbexi.timeline.server.openBEXI_timeline -data_path ""
+
+::pause
---
+++ openbexi_timeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set your environment variables here if needed
+if [ -z "$JAVA_HOME" ]
+then
+    # JAVA_HOME is undefined so set JAVA_HOME here
+    export JAVA_HOME="<YOUR_JAVA_HOME_PATH>"
+else
+    echo "JAVA_HOME="$JAVA_HOME
+fi
+if [ -z "$OPENBEXI_TIMELINE_HOME" ]
+then
+    # OPENBEXI_TIMELINE_HOME is undefined so set JAVA_HOME here
+    export OPENBEXI_TIMELINE_HOME="<YOUR_OPENBEXI_TIMELINE_HOME_PATH>"
+else
+    echo "OPENBEXI_TIMELINE_HOME="$OPENBEXI_TIMELINE_HOME
+fi
+if [ -z "$OPENBEXI_TIMELINE_DATA_PATH" ]
+then
+    # OPENBEXI_TIMELINE_DATA_PATH is undefined so set JAVA_HOME here
+    export OPENBEXI_TIMELINE_DATA_PATH="<YOUR_OPENBEXI_TIMELINE_DATA_PATH>"
+else
+    echo "OPENBEXI_TIMELINE_DATA_PATH="$OPENBEXI_TIMELINE_DATA_PATH
+fi
+
+
+$JAVA_HOME/bin/java -classpath $OPENBEXI_TIMELINE_HOME/lib/openbexi_timeline.jar com.openbexi.timeline.server.openBEXI_timeline -data_path $OPENBEXI_TIMELINE_DATA_PATH
