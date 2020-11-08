d6959ee3464f25fb35c8c50cd9b194616e17d0d1
ARCAZJ <arcazj@aol.com>
2020-11-08 15:22:38 -0500

    Added features in openBexi Timeline to handle in real time updates from the web server.

---
+++ out/production/openbexi_timeline/src/openbexi_timeline_texture.js
@@ -0,0 +1,26 @@
+let ob_texture = new Map();
+// global texture
+ob_texture.set("icon/ob_error.png", new THREE.TextureLoader().load("icon/ob_error.png"));
+ob_texture.set("icon/ob_warning.png", new THREE.TextureLoader().load("icon/ob_warning.png"));
+ob_texture.set("icon/ob_info.png", new THREE.TextureLoader().load("icon/ob_info.png"));
+ob_texture.set("icon/ob_start.png", new THREE.TextureLoader().load("icon/ob_start.png"));
+ob_texture.set("icon/ob_stop.png", new THREE.TextureLoader().load("icon/ob_stop.png"));
+ob_texture.set("icon/ob_check_failed.png", new THREE.TextureLoader().load("icon/ob_check_failed.png"));
+ob_texture.set("icon/ob_check_ok.png", new THREE.TextureLoader().load("icon/ob_check_ok.png"));
+ob_texture.set("icon/ob_check_warning.png", new THREE.TextureLoader().load("icon/ob_check_warning.png"));
+ob_texture.set("icon/ob_phone.png", new THREE.TextureLoader().load("icon/ob_phone.png"));
+ob_texture.set("icon/ob_bug.png", new THREE.TextureLoader().load("icon/ob_bug.png"));
+ob_texture.set("icon/ob_lost_connection.png", new THREE.TextureLoader().load("icon/ob_lost_connection.png"));
+ob_texture.set("icon/ob_swap.png", new THREE.TextureLoader().load("icon/ob_swap.png"));
+ob_texture.set("icon/ob_gate_open.png", new THREE.TextureLoader().load("icon/ob_gate_open.png"));
+ob_texture.set("icon/ob_gate_close.png", new THREE.TextureLoader().load("icon/ob_gate_close.png"));
+
+// Satellite/Communication texture
+ob_texture.set("icon/ob_connect.png", new THREE.TextureLoader().load("icon/ob_connect.png"));
+ob_texture.set("icon/ob_no_connect.png", new THREE.TextureLoader().load("icon/ob_no_connect.png"));
+ob_texture.set("icon/ob_satellite.png", new THREE.TextureLoader().load("icon/ob_satellite.png"));
+ob_texture.set("icon/ob_no_satellite.png", new THREE.TextureLoader().load("icon/ob_no_satellite.png"));
+ob_texture.set("icon/ob_no_tlm_red.png", new THREE.TextureLoader().load("icon/ob_no_tlm_red.png"));
+ob_texture.set("icon/ob_tlm_red.png", new THREE.TextureLoader().load("icon/ob_tlm_red.png"));
+ob_texture.set("icon/ob_tlm_green.png", new THREE.TextureLoader().load("icon/ob_tlm_green.png"));
+ob_texture.set("icon/ob_tlm_orange.png", new THREE.TextureLoader().load("icon/ob_tlm_orange.png"));
